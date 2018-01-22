81e9cf47c206d5ab02c74a7fc21b58e1377bad68
Ascherion <johan.desplats@gmail.com>
2018-01-22 15:33:29 +0100

    resultatex1

--- resultat.php
+++ resultat.php
@@ -4,7 +4,6 @@
 	<title>Resultats</title>
 	<meta charset="utf-8" />
 		<title>Hey!!!</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
 </head>
 	<body>
 		<?php
@@ -21,14 +20,9 @@
 
 		else
 		{
-			echo "Vous etes bien sur la page des resultats... Et... C'est a peu près tout.";
+			echo "Vous etes bien sur la page des resultats... Et... C'est a peu près tout!";
 		}
 		?>
-
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js">
-		</script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js">
 		</script>
 	</body>
 </html>
